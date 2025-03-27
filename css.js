1959474d7307426aea37dfc164cecac41dceb18e
Vinayak1992006 <vinayakayatti2006@gmail.com>
2025-03-27 15:43:30 +0530

    Add initial project files including HTML, CSS, and JavaScript

---
+++ css.js
@@ -0,0 +1,34 @@
+//Arthmetic operators
+let a=100;
+let b=200;
+console.log(a+b);
+console.log("a-b");
+console.log("a*b");
+console.log("a/b");
+console.log("a%b");
+console.log("a**b");
+console.log("a++");
+console.log("a--");
+
+//logical operators
+let x=true;
+let y=false;
+console.log(x&&y);
+console.log(x||y);
+console.log(!x);
+
+//comparison operators
+console.log(c==d);
+console.log(c!=d);
+console.log(c>d);
+console.log(c<d);
+console.log(c>=d);
+console.log(c<=d);
+
+//string 
+let name="sai";
+console.log(firstname);
+console.log(lastname);
+//string method
+console.log(firstname.length);
+console.log(firstname.indexof('0 '));
